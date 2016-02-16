8586ce01c1c03ab270413e30be65cece870199ab
chi.chen <chi.chen@qunar.com>
2016-02-16 14:05:48 +0800

    common test

--- src/main/java/basic/BaseTest.java
+++ src/main/java/basic/BaseTest.java
@@ -8,7 +8,18 @@ package basic;
  * @time 下午5:24
  */
 public class BaseTest {
+    /**
+     * 构造器绝不能调用可被覆盖的方法
+     */
+    public BaseTest() {
+        hi();
+    }
+
     protected void hi() {
         System.out.println("hi");
     }
+
+    /*
+     * public final void hello() { System.out.println("hello"); }
+     */
 }
--- src/main/java/basic/DerivedTest.java
+++ src/main/java/basic/DerivedTest.java
@@ -8,8 +8,25 @@ package basic;
  * @time 下午5:25
  */
 public class DerivedTest extends BaseTest {
+    private int counter;
+
+    public DerivedTest(int counter) {
+        this.counter = counter;
+    }
+
+    public static void main(String[] args) {
+        BaseTest baseTest = new DerivedTest(5);
+        baseTest.hi();
+    }
+
+    /* 方法也可以用final来修饰，不可被重写 */
+    /*
+     * public void hello() { System.out.println("hi, derived"); }
+     */
+
     @Override
     public void hi() {
-        super.hi();
+        System.out.println("hello");
+        System.out.println("counter: " + counter);
     }
 }
