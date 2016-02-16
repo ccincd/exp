90e7c53d98be5557c6e383f410e5652db357cf83
chi.chen <chi.chen@qunar.com>
2016-02-16 17:08:33 +0800

    getDeclaredMethod Test

--- src/main/java/toy/proxy/DummyProxy.java
+++ src/main/java/toy/proxy/DummyProxy.java
@@ -1,9 +1,9 @@
 package toy.proxy;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,19 +21,48 @@ public class DummyProxy<T> implements InvocationHandler {
         this.list = list;
     }
 
-    public static void main(String[] args) {
-        List<String> myList = new ArrayList<String>();
-        myList.add("hi");
-        myList.add("there");
-        DummyProxy<String> dummyProxy = new DummyProxy<String>(myList);
-        List<String> proxyList = dummyProxy.getProxyInstance();
+    public DummyProxy() {
+    }
 
+    public static void main(String[] args) {
+        /**
+         * 动态代理测试
+         */
+        /*
+         * List<String> myList = new ArrayList<String>(); myList.add("hi"); myList.add("there"); DummyProxy<String>
+         * dummyProxy = new DummyProxy<String>(myList); List<String> proxyList = dummyProxy.getProxyInstance(); try {
+         * System.out.println(proxyList.size()); proxyList.add("hi"); } catch (Exception e) { e.printStackTrace(); }
+         */
+
+        /**
+         * getDeclaredMethod测试
+         */
+        DummyProxy<String> dummyProxy = new DummyProxy<String>();
+        dummyProxy.invokeHi("chichen", 28);
+    }
+
+    public void invokeHi(String name, int age) {
+        Method sayHi;
         try {
-            System.out.println(proxyList.size());
-            proxyList.add("hi");
-        } catch (Exception e) {
+            sayHi = this.getClass().getDeclaredMethod("hi", String.class, Integer.class);
+        } catch (NoSuchMethodException e) {
+            sayHi = null;
             e.printStackTrace();
         }
+
+        if (sayHi != null) {
+            try {
+                sayHi.invoke(this, name, age);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void hi(String name, Integer age) {
+        System.out.println("hi, there" + " I'm " + name + ", and I'm " + age + " years old.");
     }
 
     @SuppressWarnings("unchecked")
