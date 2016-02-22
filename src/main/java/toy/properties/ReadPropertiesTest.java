feb513967c4bb04154147ce053e65ffb7b904801
chi.chen <chi.chen@qunar.com>
2016-02-22 19:47:23 +0800

    properties test

---
+++ src/main/java/toy/properties/ReadPropertiesTest.java
@@ -0,0 +1,26 @@
+package toy.properties;
+
+import java.io.InputStream;
+
+/**
+ * ReadPropertiesTest
+ *
+ * @author chi.chen
+ * @date 16-2-22
+ * @time 下午6:25
+ */
+public class ReadPropertiesTest {
+
+    private static final String resourceName = "ApplicationResources.properties";
+
+    public static void main(String[] args) {
+        InputStream inputStreamGlobal = ReadPropertiesTest.class.getClassLoader().getResourceAsStream(resourceName);
+        // InputStream inputStreamLocal = ReadPropertiesTest.class.getResourceAsStream(resourceName);
+        InputStream inputStreamLocal = ReadPropertiesTest.class.getResourceAsStream("/toy/properties/" + resourceName);
+        String packagePath = ReadPropertiesTest.class.getResource("").getPath();
+
+        System.out.println(packagePath);
+        System.out.println(inputStreamGlobal);
+        System.out.println(inputStreamLocal);
+    }
+}
---
+++ src/main/test/java/test/ReadPropertiesTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.io.InputStream;
+
+import org.junit.Test;
+
+import common.BaseTest;
+
+/**
+ * ReadPropertiesTest
+ *
+ * @author chi.chen
+ * @date 16-2-22
+ * @time 下午6:30
+ */
+public class ReadPropertiesTest extends BaseTest {
+
+    private static final String resourceName = "ApplicationResources.properties";
+
+    @Test
+    public void testResourceLoading() {
+        InputStream inputStreamGlobal = ReadPropertiesTest.class.getClassLoader().getResourceAsStream(resourceName);
+        InputStream inputStreamLocal = ReadPropertiesTest.class.getResourceAsStream(resourceName);
+        String packagePath = this.getClass().getResource("").getPath();
+
+        System.out.println(packagePath);
+        System.out.println(inputStreamGlobal);
+        System.out.println(inputStreamLocal);
+    }
+}
