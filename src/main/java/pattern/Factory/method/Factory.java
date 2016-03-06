fc8933f4274bbe95a4a6d232dd48423ab803b9bd
chi.chen <984016717@qq.coom>
2016-03-06 23:44:32 +0800

    抽象工厂模式

--- src/main/java/pattern/Factory/method/Factory.java
+++ src/main/java/pattern/Factory/method/Factory.java
@@ -4,6 +4,8 @@ package pattern.factory.method;
  * 工厂方法接口,每个具体的工厂类均需要实现该接口
  * 方便横向扩展,但纵向扩展时仍需要修改原代码
  *
+ * 工厂模式返回的实例可以不是新创建的，返回由工厂创建好的实例也是可以的
+ *
  * Created by cc on 16/3/6.
  */
 public interface Factory {
