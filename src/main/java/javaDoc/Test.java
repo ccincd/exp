ef325499cc97e8b203591d27b4d9fa5f3615e61e
chi.chen <chi.chen@qunar.com>
2016-03-08 10:06:57 +0800

    ul li code

---
+++ src/main/java/javaDoc/Test.java
@@ -0,0 +1,18 @@
+package javaDoc;
+
+/**
+ * JavaDoc测试类
+ *
+ * <br/>
+ * <ul>
+ * <li>第一点JavaDoc说明</li>
+ * <li>第二点<code>JavaDoc说明</code></li>
+ * <li>第三点说明</li>
+ * </ul>
+ *
+ * @author chi.chen
+ * @date 16-3-8
+ * @time 上午10:02
+ */
+public class Test {
+}
