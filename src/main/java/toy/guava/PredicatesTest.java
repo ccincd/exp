153ebc4567a3bd5dffb6c42c1e4de3c5a763a951
chi.chen <chi.chen@qunar.com>
2016-03-14 11:36:38 +0800

    PredicatesTest

--- src/main/java/toy/guava/PredicatesTest.java
+++ src/main/java/toy/guava/PredicatesTest.java
@@ -1,8 +1,12 @@
 package toy.guava;
 
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
 
 /**
  * PredicatesTest
@@ -27,11 +31,26 @@ public class PredicatesTest {
     protected static Predicate<Person> filterAge = new Predicate<Person>() {
         @Override
         public boolean apply(Person input) {
-            return false;
+            if (input == null) {
+                return false;
+            }
+
+            return input.getAge() > 0 && input.getAge() < 40;
         }
     };
 
     public static void main(String[] args) {
-        //
+        List<Person> persons = Lists.newArrayList();
+        persons.add(new Person("jackson", 1, 35));
+        persons.add(new Person("rose", 2, 25));
+        persons.add(new Person("ada wong", 2, 50));
+
+        for (Person person : persons) {
+            boolean flag = Predicates.and(filterName, filterAge).apply(person);
+
+            if (flag) {
+                System.out.println(person);
+            }
+        }
     }
 }
