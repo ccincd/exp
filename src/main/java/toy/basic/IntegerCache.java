162581a476df659b1c61e28936100d66d96c1281
chi.chen <chi.chen@qunar.com>
2016-03-18 10:09:02 +0800

    IntegerCache

---
+++ src/main/java/toy/basic/IntegerCache.java
@@ -0,0 +1,21 @@
+package toy.basic;
+
+/**
+ * IntegerCache
+ *
+ * @author chi.chen
+ * @date 16-3-18
+ * @time 上午10:07
+ */
+public class IntegerCache {
+
+    public static void main(String[] args) {
+        Integer a = 1000;
+        Integer b = 1000;
+        Integer c = 100;
+        Integer d = 100;
+
+        System.out.println(a == b);
+        System.out.println(c == d);
+    }
+}
