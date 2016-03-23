77c2222f072d7f98c5c31c554335823c427d3002
chi.chen <chi.chen@qunar.com>
2016-03-23 12:01:42 +0800

    JodaTimeDateTest

---
+++ src/main/java/toy/time/JodaTimeDateTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016 Qunar.com. All Rights Reserved.
+ */
+package toy.time;
+
+import org.joda.time.LocalDate;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author chichen Date: 16-3-23 Time: 上午11:56
+ */
+public class JodaTimeDateTest {
+
+    public static void main(String[] args) {
+        String oneDay = "2016-03-15";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date = sdf.parse(oneDay);
+            System.out.println(date);
+
+            LocalDate localDate = new LocalDate(date);
+            System.out.println(localDate.toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
