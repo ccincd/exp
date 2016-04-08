4773ca93f333275cc4a1b656929536d423047e70
chi.chen <chi.chen@qunar.com>
2016-04-08 14:55:24 +0800

    整理import

--- src/main/java/toy/time/java/DateAndTimestampTest.java
+++ src/main/java/toy/time/java/DateAndTimestampTest.java
@@ -3,7 +3,6 @@
  */
 package toy.time.java;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import controller.ApiResult;
