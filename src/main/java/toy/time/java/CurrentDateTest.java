9c1702697d642cddb1ca9712d7759bc248fd9c45
chi.chen <chi.chen@qunar.com>
2016-04-08 14:51:51 +0800

    CurrentDateTest

---
+++ src/main/java/toy/time/java/CurrentDateTest.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Qunar.com. All Rights Reserved.
+ */
+package toy.time.java;
+
+import java.util.Date;
+
+/**
+ * @author chichen  Date: 16-4-8 Time: 下午2:50
+ */
+public class CurrentDateTest {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
