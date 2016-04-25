5b4067683c5036819f39a7fcd8481564b578baa9
chi.chen <chi.chen@qunar.com>
2016-04-25 15:48:54 +0800

    add comment

--- src/main/java/proxy/dynamic/WelcomeInvocationHandler.java
+++ src/main/java/proxy/dynamic/WelcomeInvocationHandler.java
@@ -19,6 +19,33 @@ public class WelcomeInvocationHandler implements InvocationHandler {
         this.concreteClass = concreteClass;
     }
 
+    /**
+     * 可根据方法名称（如使用正则表达式）或参数类型、个数等来区分不同的处理方式
+     *
+
+     public Object invoke(Object proxy, Method method, Object[] args)
+        throws Throwable {
+        Type[] types = method.getParameterTypes();
+        if (method.getName().matches("get.+") && (types.length == 1) &&
+            (types[0] == String.class)) {
+            String key = (String) args[0];
+            Object value = cached.get(key);
+            if (value == null) {
+                value = method.invoke(target, args);
+                cached.put(key, value);
+            }
+            return value;
+        }
+        return method.invoke(target, args);
+     }
+
+     *
+     * @param proxy 被代理类
+     * @param method 被代理类的方法
+     * @param args 被代理类的参数
+     * @return 方法执行结果
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         System.out.println("do something before invocation");
