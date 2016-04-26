4dc33ee0ee3a90d031b35acd441676bd1def1048
chi.chen <chi.chen@qunar.com>
2016-04-26 18:47:34 +0800

    CGLib book base demo

---
+++ src/main/java/proxy/cglib/BookService.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016 Qunar.com. All Rights Reserved.
+ */
+package proxy.cglib;
+
+/**
+ * @author chichen  Date: 16-4-26 Time: 下午6:32
+ */
+public class BookService {
+
+    public void create() {
+        System.out.println("create()..");
+    }
+
+    public void query() {
+        System.out.println("query()..");
+    }
+
+    public void update() {
+        System.out.println("update()..");
+    }
+
+    public void delete() {
+        System.out.println("delete()..");
+    }
+}
