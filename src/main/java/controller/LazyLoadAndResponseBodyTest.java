ad1cdbee1e6f48d53848607c2a7278036afb7072
chi.chen <984016717@qq.coom>
2016-05-02 12:49:07 +0800

    ExceptionHandler

--- src/main/java/controller/LazyLoadAndResponseBodyTest.java
+++ src/main/java/controller/LazyLoadAndResponseBodyTest.java
@@ -30,6 +30,17 @@ public class LazyLoadAndResponseBodyTest {
             }
         });
 
+        /**
+         * 1、MappingJackson2HttpMessageConverter和AbstractJackson2HttpMessageConverter会执行序列化操作
+         *
+         * 2、序列化过程如果抛出异常则会进入DispatcherServlet的processHandlerException异常处理函数中
+         *
+         * 3、DispatcherServlet有一个handlerExceptionResolvers异常处理列表，其中包含自定义的GlobalExceptionHandler，
+         * 它实现了HandlerExceptionResolver接口，但异常处理是按责任链的方式进行处理，而GlobalExceptionHandler又处在责任链
+         * 的底端，所以异常被DefaultHandlerExceptionResolver处理掉，给前端返回了500错误码和异常信息
+         *
+         * 4、解决办法是让GlobalExceptionHandler实现Ordered接口，重写获取order的方法，设置高优先级
+         */
         return ApiResult.succ(dividedNumbers);
     }
 }
