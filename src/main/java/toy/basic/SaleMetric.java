7dbd61bb4ff31263f0b06e40980fe0b0718e03e5
陈驰 <chenchi@yingwumeijia.com>
2016-08-09 15:43:51 +0800

    hi

--- src/main/java/toy/basic/SaleMetric.java
+++ src/main/java/toy/basic/SaleMetric.java
@@ -12,11 +12,17 @@ public enum SaleMetric implements EnumTrait {
 
     PAYED_COUNT(1, "成单量"),
     ROOM_NIGHT_COUNT(2, "订单间夜数"),
+    HI(8, "hi"),
     SALE_AMOUNT(3, "销售额"),
     REFUND_AND_CANCEL_COUNT(4, "退款/取消数"),
     REJECT_COUNT(5, "拒单数"),
     USED_PAYED_COUNT(6, "实住订单数"),
     USED_ROOM_NIGHT_COUNT(7, "实住间夜数");
+
+    public static void main(String[] args) {
+        SaleMetric[] saleMetrics = SaleMetric.values();
+        System.out.println(saleMetrics);
+    }
 
     private final Integer code;
 
